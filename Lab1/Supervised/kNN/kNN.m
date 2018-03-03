92a29cfdc195bbe1f2f5f13362011e4164b8e144
Sebastian Bångerius <bangerius@bitbucket.org>
2018-03-03 02:57:12 +0100

    implemented KNN

--- Lab1/Supervised/kNN/kNN.m
+++ Lab1/Supervised/kNN/kNN.m
@@ -9,10 +9,33 @@ function [ labelsOut ] = kNN(X, k, Xt, Lt)
 %   Output:
 %               LabelsOut = Vector with the classified labels
 
-labelsOut  = zeros(size(X,2),1);
-classes = unique(Lt);
-numClasses = length(classes);
-
-
+%Calculate distances
+distances = zeros(size(X,2),size(Xt,2));
+for i = 1:size(X,2)
+    for j = 1:size(Xt,2)
+        distances(i,j)=euclideanDist(X(:,i),Xt(:,j));
+    end
 end
 
+%Get classes for nearest neighbors
+nns = zeros(size(X,2),k);
+for i = 1:size(X,2)
+    [~,sortIndex] = sort(distances(i,:),'ascend');
+    minIndex = sortIndex(1:k);
+    nns(i,:) = Lt(minIndex);
+end
+
+%Majority vote
+labelsOut = mode(nns,2);
+
+%labelsOut = nns;
+end
+
+function [ dist ] = euclideanDist(x1, x2)
+        dim = length(x1);
+        sum = 0;
+        for i = 1:dim
+            sum = sum+(x1(i)-x2(i))^2;
+        end
+        dist = sqrt(sum);
+end
